f192bda3ca8c6001c531664f97efe126ed6ed8a0
umberto <umberto.onraghi@gmail.com>
2025-05-19 12:21:00 +0200

    add configuration to append in local IDE (Intellij) Kafka plugin to monitor Kafka

--- order-service/src/main/java/com/example/orderservice/configuration/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/configuration/KafkaProducerConfig.java
@@ -1,15 +1,17 @@
 package com.example.orderservice.configuration;
 
+import com.example.orderservice.dto.OrderCreatedEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
 import java.util.HashMap;
 import java.util.Map;
-import com.example.orderservice.dto.OrderCreatedEvent;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.kafka.core.*;
 
 
 @Configuration
@@ -18,7 +20,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, OrderCreatedEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
